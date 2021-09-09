cf10c22c3c6abb06c56d4d11064f81172218f8bd
smpln <79950920+smpln@users.noreply.github.com>
2021-09-09 16:13:33 +0800

    modul duta

---
+++ database/factories/DutaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Duta;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DutaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Duta::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
